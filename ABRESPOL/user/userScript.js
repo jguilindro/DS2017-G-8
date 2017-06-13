cfe9abc53075aaa38b9b243130f89d450d4fb51e
Xidrovo <xidrovo@hotmail.com>
2017-06-12 19:17:41 -0500

    filtrado cosillas

--- ABRESPOL/user/userScript.js
+++ ABRESPOL/user/userScript.js
@@ -1,7 +1,52 @@
-$(document).ready(function() {
+var App = new Vue({
+  mounted: function(){
+    //Inicializaciones de Materializecss
     $('select').material_select();
-  });
+  },
+  el: '#app',
+  data: {
+    todoElMenu: info,
+    filteredMenu: info,
+    facultad: "",
+    textoFiltro: ""
+  },
+  methods: {
+	  	cambiarCategoria: function(){
+	  		alert("asdasd");
+	  	}
+	}
+});
 
 $(".logout").click(function(){
 	window.location.href = '../index.html';
 })
+
+$("#categoria").change(function(){
+	App.facultad = $(this).val()
+	if (App.facultad != "")
+		App.filteredMenu = App.todoElMenu.filter(filtrarFacultad);
+	else
+		App.filteredMenu = App.todoElMenu;
+})
+
+$("#plato").on("input", function(){
+	App.textoFiltro = this.value;
+	App.filteredMenu = App.todoElMenu.filter(filter);
+	console.log(App.filteredMenu)
+})
+
+function filter(menu){
+	var nombre = menu.nombre.match(App.textoFiltro);
+	var entrada = menu.entrada.nombre.match(App.textoFiltro);
+	var fuerte = menu.Fuerte.nombre.match(App.textoFiltro);
+	var postre = menu.Postre.nombre.match(App.textoFiltro);
+
+	if(nombre != null || entrada != null || fuerte != null || postre != null)
+		return true
+	else
+		return false
+}
+
+function filtrarFacultad(menu){
+	return menu.Lugar == App.facultad
+}
