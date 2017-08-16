29119472b6d235de25fbcc310c708a5c9875bf0e
Xidrovo <xidrovo@hotmail.com>
2017-08-16 12:36:17 -0500

    mire profe trabajamos :dancers:

---
+++ OO/ABRESPOL/ABRESPOL/Facade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABRESPOL
+{
+    class Facade
+    {
+        public bool pedirPlato() {
+            Console.WriteLine("asdsad");
+            return true;
+        }
+        public bool realizarPago() {
+            return false;
+        }
+        public float verSaldo() {
+            return 5.2f;
+        }
+    }
+}
